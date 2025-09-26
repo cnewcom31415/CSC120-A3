93012a7916f8ebdfa756024f0c005ef22546eca3
cnewcom31415 <cnewcom@smith.edu>
2025-09-25 23:04:52 -0400

    Commit 1

--- Conversation.java
+++ Conversation.java
@@ -1,19 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * A class that simulates a conversation with a Chat Bot
+ */
 class Conversation implements ConversationRequirements {
+
 
   // Attributes 
 
+  private ArrayList<String> transcript;
+  private HashMap<String,String> mirrorDict;
+  private static ArrayList<String> cannedResponses = new ArrayList<>();
+
   /**
-   * Constructor 
+   * Constructs an instance of the Conversation class.
    */
   Conversation() {
-    
+    this.transcript = new ArrayList<>();
+    this.mirrorDict = new HashMap<>();
+    mirrorDict.put(" i'm ", " you're_ ");
+    mirrorDict.put(" you ", " I_ "); //works
+    mirrorDict.put(" me ", " you_ ");
+    mirrorDict.put(" am ", " are_ ");//works
+    mirrorDict.put(" are ", " am_ ");
+    mirrorDict.put(" I ", " you_ ");
+    mirrorDict.put(" your ", " my_ ");
+    mirrorDict.put(" my ", " your_ ");
+    cannedResponses.add("Oh yeah!");
+    cannedResponses.add("I'm sorry");
+    cannedResponses.add("Mmhmm");
+    cannedResponses.add("Okay");
   }
 
   /**
-   * Starts and runs the conversation with the user
+   * Starts and runs the conversation with the user and records the transcript
    */
   public void chat() {
+    
+    Scanner scan = new Scanner(System.in);
+    String input = "";
+    String response = "";
+    
+    System.out.println("Hello! Please input how many rounds as an int:");
+    input = scan.nextLine();
+    
+    System.out.println("\n\n\nHi there! What's on your mind?");
+    this.transcript.add("Hi there! What's on your mind?");
+      
+    int rounds = Integer.parseInt(input);
+    
+    for(int i=1; i<=rounds; i++){
+    
+      input = scan.nextLine();
+      this.transcript.add(input);
+      response = this.respond(input);
+      System.out.println(response);
+      this.transcript.add(response);
+      
+    }
+
+    System.out.println("See you later!");
+    this.transcript.add("See you later!");
+
+    scan.close();
 
   }
 
@@ -21,7 +73,11 @@ class Conversation implements ConversationRequirements {
    * Prints transcript of conversation
    */
   public void printTranscript() {
+    System.out.println("\n\n\nTRANSCRIPT");
 
+    for(String line : this.transcript){
+      System.out.println(line);
+    }
   }
 
   /**
@@ -30,7 +86,44 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    
+    // Setting up the strings
+    String passingString = " " + inputString.toLowerCase() + " "; // Spaces are put there so each word gets changed
+    String qualityControlString = " " + inputString.toLowerCase() + " ";
+
+    if(passingString.contains(" i ")){
+      passingString = passingString.replaceAll(" i ", " I ");
+      qualityControlString = qualityControlString.replaceAll(" i ", " I ");
+      passingString = passingString.replaceAll(" i ", " I ");
+      qualityControlString = qualityControlString.replaceAll(" i ", " I ");
+      // done twice because there was an issue if the same word was repeated over and over
+    }
+
+    int count = 0;
+
+    // Mirroring the strings if possible
+    for(String str: mirrorDict.keySet()){
+
+      //Mirrors String, everything lowercase
+      if(qualityControlString.contains(str)){
+        String change = mirrorDict.get(str);
+        passingString = passingString.replaceAll(str, change);
+        passingString = passingString.replaceAll(str, change); // done twice because there was an issue if the same word was repeated over and over
+        qualityControlString = qualityControlString.replace(str,"   ");
+      }
+      else{
+        count++;
+      }
+    }
+
+    String returnString = passingString.substring(1).replaceAll("_", ""); // Gets rid of empty space
+
+    Random randy = new Random();
+    //Canned responses
+    if(count == mirrorDict.keySet().size()){
+      returnString = cannedResponses.get(randy.nextInt(0, cannedResponses.size()));
+    }
+
     return returnString; 
   }
 
--- reflection.md
+++ reflection.md
@@ -5,3 +5,13 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+1. I did not consult with any classmates for this assignment
+2. I did not attend any sessions, because I didn't have any time this week. In the future, though, I want to go to more office hours
+3. I've coded in Java before, but I do prefer it in comparison to Python. Needing to declare variables as specific types is actually very helpful in my brain, because I can keep track of what is supposed to be what.
+4. Python seems to be better at smaller programming problems, and Java does well with big programming problems that require a lot of interconnecting parts, but is more tedious when doing smaller problems. I think the conclusion I would draw from this is that if one coding language does not work for one problem, there's probably something out there that has the functionality you want
+
+
+5. The transcript worked pretty well. The main method automatically called it, but if that was not there I would have just called transcript in the chat method. The hardest part was making sure each of the different mirror words were accurately mirrored. I tried to figure out capitalization and punctuation, but it became very confusing to try and make work. If I had more time I think I would have tried to make a helper function to modify the string in terms of capitalization and punctuation.
+
+My advise: Apparently VS code thinks its smart and will get rid of unused imports for you. This is very annoying because I prefer to write the import and then call the object without errors. You can fix this by unclicking Jdk>Java>On Save> Organize Imports. This means you have to organize them yourself, but you don't have to deal with imports that you are going to use disappearing on you. Also, I wish I had used helper functions to block out my code so I could try and test it better.
